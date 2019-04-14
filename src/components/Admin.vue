175160cdd1d2b302af1d73c67579802b1817cb27
vanderkilu <vanderkiliku@gmail.com>
2019-04-14 11:48:34 -0400

    adding admin page

---
+++ src/components/Admin.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="admin-wrapper">
+        <div class="nav">
+            <p class="nav__brand">Hospitally.gh</p>
+            <p class="nav__link">Hospitals</p>
+            <p class="nav__link">Add Hospital</p>
+        </div>
+    </div>
+</template>
+
+
+<script>
+import { headerMixin } from './mixins/HeaderMixin.js'
+export default {
+    mixins: [headerMixin],
+    data() {
+        return {
+            hospitals: []
+        }
+    },
+
+}
+</script>
