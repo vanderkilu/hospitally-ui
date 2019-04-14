1cae4000c5e8a5a46160ab3cac26c49c0365305e
vanderkilu <vanderkiliku@gmail.com>
2019-04-14 14:28:00 -0400

    added admin files

---
+++ src/components/AdminHome.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="admin">
+        <div class="admin__hospitals">
+            <div class="admin-hospital__card" v-for="hospital in hospitals" :key="hospital.id">
+                <p class="hospital__name">{{hospital.name}}</p>
+                <p class="hospital__location">{{hospital.location.city}}</p>
+                <img src="../assets/edit.svg" class="hospital__icon">
+                <img src="../assets/delete.svg" class="hospital__icon">
+            </div>
+        </div>
+        <app-loader v-if="$apollo.loading"></app-loader>
+    </div>
+</template>
+
+<script>
+import { HOSPITALS } from '../graphql.js'
+import Loader from './Loader.vue'
+export default {
+    data() {
+        return {
+            hospitals: [],
+        }
+    },
+    components: {
+        appLoader: Loader
+    },
+    apollo: {
+        hospitals: {
+            query: HOSPITALS,
+            update: (data) => data.hospitals
+        }
+    },
+
+}
+</script>
+
+<style scoped>
+    .admin__hospitals {
+        width: 60%;
+        margin: 2rem auto;
+        box-shadow: 0 1rem 2rem rgba(0,0,0,0.03);
+        background-color: #f5f5f5;
+        min-height: 80vh;
+    }
+    .admin-hospital__card {
+        display: flex;
+        justify-content: space-between;
+        padding: 2rem;
+        align-items: center;
+        background-color: white;
+        box-shadow: 0 1rem 2rem rgba(0,0,0,0.03);
+        margin-bottom: 0.6rem;
+    }
+    .hospital__name {
+        flex-basis: 40%
+    }
+    .hospital__location {
+         flex-basis: 30%;
+    }
+    .hospital__name, .hospital__location  {
+        font-size: 1.4rem;
+        color:#757575;
+    }
+    .hospital__icon {
+        width: 2rem;
+        height: 2rem;
+        flex-basis: 15%;
+    }
+</style>
---
+++ src/components/AdminWrapper.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="admin-wrapper">
+        <div class="nav colored">
+            <router-link class="nav__brand" :to="{name: 'home'}">Hospitally.gh</router-link>
+            <router-link class="nav__link" :to="{}">Hospitals</router-link>
+            <router-link class="nav__link" :to="{name: 'new-hospital'}">Add Hospital</router-link>
+        </div>
+        <router-view></router-view>
+    </div>
+</template>
+
+
+<script>
+import { headerMixin } from './mixins/HeaderMixin.js'
+import { getToken } from '../utils'
+export default {
+    mixins: [headerMixin],
+    data() {
+        return {
+
+        }
+    },
+    beforeRouteEnter(to, from, next) {
+        if (getToken() === null) {
+             next({name: 'login', params: {name: 'admin'}})
+        }
+        else {
+            next()
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .nav {
+        padding: 2rem;
+    }
+</style>
